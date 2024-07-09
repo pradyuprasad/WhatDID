a5e37b7998a61c94620614c07cc8fb91d3087e28
pradyuprasad <pradyu.sp@gmail.com>
2024-07-09 13:53:21 +0530

    format file

--- main.py
+++ main.py
@@ -4,48 +4,53 @@ from typing import List, Optional
 from sqlite3 import Connection, Cursor
 import subprocess
 from subprocess import CompletedProcess
-from AppKit import NSWorkspace, NSRunningApplication, NSApplicationActivationPolicyRegular # type : ignore
+from AppKit import (
+    NSWorkspace,
+    NSRunningApplication,
+    NSApplicationActivationPolicyRegular,
+)  # type : ignore
 import time
 from models import WindowData
 
 
-
-
 class DB_Cursor:
-    __cursor:Optional[Cursor] = None
+    __cursor: Optional[Cursor] = None
 
     @staticmethod
     def get_cursor() -> Cursor:
         if DB_Cursor.__cursor:
             return DB_Cursor.__cursor
         else:
-            conn:Connection = sqlite3.connect('tracker.db')
+            conn: Connection = sqlite3.connect("tracker.db")
             DB_Cursor.__cursor = conn.cursor()
             return DB_Cursor.__cursor
 
 
-def run_applescript(applescript_code:str) -> str:
-    result:CompletedProcess = subprocess.run(['osascript', '-e', applescript_code], capture_output=True, text=True)
+def run_applescript(applescript_code: str) -> str:
+    result: CompletedProcess = subprocess.run(
+        ["osascript", "-e", applescript_code], capture_output=True, text=True
+    )
     if result.stderr:
         raise RuntimeError(f"Error while running AppleScript: {result.stderr}")
     return result.stdout.strip()
 
 
 def create_tables() -> None:
-    create_table_statements: List[str] = ['''
+    create_table_statements: List[str] = [
+        """
                                           CREATE TABLE IF NOT EXISTS tags (
                                           tag_id INTEGER PRIMARY KEY, 
                                           tag_name TEXT);
-                                          ''', 
-                                          '''
+                                          """,
+        """
                                         CREATE TABLE IF NOT EXISTS sessions (
                                         session_id INTEGER PRIMARY KEY,
                                         session_name TEXT,
                                         session_start INTEGER, 
                                         session_end INTEGER
                                         );
-                                        ''', 
-                                        '''
+                                        """,
+        """
                                         CREATE TABLE IF NOT EXISTS activity (
                                         acitivty_id INTEGER PRIMARY KEY,
                                         application_name TEXT NOT NULL,
@@ -59,35 +64,39 @@ def create_tables() -> None:
                                         FOREIGN KEY (tag_id) REFERENCES tags (tag_id),
                                         FOREIGN KEY (session_id) REFERENCES sessions (session_id)
                                         );
-                                        ''']
+                                        """,
+    ]
     try:
-        cursor:Cursor = DB_Cursor.get_cursor()
+        cursor: Cursor = DB_Cursor.get_cursor()
         for statement in create_table_statements:
             cursor.execute(statement)
     except Exception as e:
         print(e)
         raise e
 
+
 def get_active_window() -> WindowData:
     # Get the currently active (frontmost) application
     workspace = NSWorkspace.sharedWorkspace()
     active_app = workspace.activeApplication()
     print(active_app)
-    app_name = active_app['NSApplicationName']
-    
+    app_name = active_app["NSApplicationName"]
+
     # Get the frontmost app
-    frontmost_app = NSRunningApplication.runningApplicationWithProcessIdentifier_(active_app['NSApplicationProcessIdentifier'])
-    
+    frontmost_app = NSRunningApplication.runningApplicationWithProcessIdentifier_(
+        active_app["NSApplicationProcessIdentifier"]
+    )
+
     # Try to get the window title
     if frontmost_app.activationPolicy() == NSApplicationActivationPolicyRegular:
         # Use AppleScript to get the window title
-        script = f'''
+        script = f"""
         tell application "System Events"
             tell process "{app_name}"
                 return name of first window
             end tell
         end tell
-        '''
+        """
         try:
             window_title = run_applescript(script)
         except subprocess.CalledProcessError:
@@ -95,21 +104,16 @@ def get_active_window() -> WindowData:
     else:
         window_title = "No window"
 
-    
     print(f"Application: {app_name}")
     print(f"Window Title: {window_title}")
     return WindowData(application_name=app_name, title=window_title)
 
 
-
 def main() -> None:
     create_tables()
     while True:
-        #currWindownData:WindowData =  get_active_window()
+        # currWindownData:WindowData =  get_active_window()
         time.sleep(1)
-
-
-
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -2,12 +2,14 @@ from enum import Enum
 from pydantic import BaseModel
 from typing import Optional
 
+
 class Browser(Enum):
-    SAFARI = 'Safari'
-    CHROME = 'Google Chrome'
-    FIREFOX = 'Firefox'
-    BRAVE = 'Brave Browser'
-    EDGE = 'Microsoft Edge'
+    SAFARI = "Safari"
+    CHROME = "Google Chrome"
+    FIREFOX = "Firefox"
+    BRAVE = "Brave Browser"
+    EDGE = "Microsoft Edge"
+
 
 class WindowData(BaseModel):
     application_name: Optional[str]
